2bbb8af48bdc5a653f09ca83dc6ef826fd22f8a9
Berend Weij <info@berendweij.nl>
2018-11-13 13:20:37 +0100

    adds project setup

---
+++ Assets/Scripts/WebcamMaterial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamMaterial : MonoBehaviour
+{
+    [SerializeField] private int width = 1280;
+    [SerializeField] private int height = 720;
+    [SerializeField] private int fps = 30;
+
+    public int Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public int Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+
+    public int FPS
+    {
+        get { return fps; }
+        set { fps = value; }
+    }
+
+    void Start()
+    {
+        var webcamTexture = new WebCamTexture(WebCamTexture.devices[0].name, Width, Height, FPS);
+        var renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        var aspectRatio = Width / (float) Height;
+        transform.localScale = new Vector3(transform.localScale.x * aspectRatio, transform.localScale.y,
+            transform.localScale.z);
+        webcamTexture.Play();
+    }
+}
