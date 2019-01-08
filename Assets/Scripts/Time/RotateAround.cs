7f91d04d520cc589b6bbebaff7e05e18b5f92e88
Gijs Schouten <21969@ma-web.nl>
2019-01-08 12:36:43 +0100

    Day-night input movement progress

---
+++ Assets/Scripts/Time/RotateAround.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+	[SerializeField] private GameObject _axis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (Input.GetKey(KeyCode.D)) {
+			Move(1);
+		}
+
+	    if (Input.GetKey(KeyCode.A)) {
+			Move(-1);
+		}
+
+	    /*switch (Input.inputString) {
+			case "d":
+				transform.RotateAround(Vector3.zero, Vector3.forward, 1);
+				print("D");
+				break;
+
+			case "a":
+				transform.RotateAround(Vector3.zero, Vector3.forward, -1);
+				break;
+		}*/
+	}
+
+	private void Move(float angle) {
+		transform.RotateAround(Vector3.zero, Vector3.forward, angle);
+	}
+}
