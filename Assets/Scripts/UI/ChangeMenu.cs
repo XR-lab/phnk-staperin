19e285823d2bbf13b33d7f2b82226bc9cbc652d7
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-01-15 16:35:03 +0100

    Added ChangeMenu

---
+++ Assets/Scripts/UI/ChangeMenu.cs
@@ -0,0 +1,11 @@
+﻿using CM.UI;
+using UnityEngine;
+
+public class ChangeMenu : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown("Trackpad"))
+			GetComponent<CM_UI_System>().SwitchScreens(null);
+	}
+}
