dd2681e83aa85c75b439117c0afa2e6e5cd5e322
Berend Weij <info@berendweij.nl>
2019-01-23 22:06:40 +0100

    adds new magnifier interaction mode

---
+++ Assets/Scripts/States/Magnifier.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Magnifier : State<InteractionStates>
+{
+    public override InteractionStates Id => InteractionStates.Magnifier;
+
+    [SerializeField] private Material _targetMaterial;
+
+    public Material TargetMaterial => _targetMaterial;
+
+    public override void Apply(float amount) {
+        var magnification = 1f + amount * 2;
+        TargetMaterial.SetFloat("_Magnification", magnification);
+    }
+}
