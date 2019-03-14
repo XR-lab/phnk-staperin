d8e56896f72c281fe93f50a595243a0c0c18a7ee
Tommiee <tomdepom1999@gmail.com>
2019-03-14 14:02:12 +0100

    changed script to scroll through instead of pick states

--- Assets/Scripts/UI/KeyboardChangeState.cs
+++ Assets/Scripts/UI/KeyboardChangeState.cs
@@ -1,28 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CM.UI;
 
 public class KeyboardChangeState : MonoBehaviour
 {
 	private readonly StateMachine<InteractionStates> _interactionMachine = new StateMachine<InteractionStates>();
+	private CM_UI_System_ScreenRotation _uiSystemScreenRotation;
+	private Dictionary<string, InteractionStates> _interactionStates = new Dictionary<string, InteractionStates>(){
+		{ "DayNightUI", InteractionStates.DayNight},
+		{ "HiddenLayerUI", InteractionStates.HiddenLayer},
+		{ "MagnifierUI", InteractionStates.Magnifier},
+		{ "ScalerUI", InteractionStates.Scaler}
+	};
 
     void Update(){
 		bool _keyOne = Input.GetKeyDown(KeyCode.Alpha1);
 		bool _keyTwo = Input.GetKeyDown(KeyCode.Alpha2);
-		bool _keyThree = Input.GetKeyDown(KeyCode.Alpha3);
-		bool _keyFour = Input.GetKeyDown(KeyCode.Alpha4);
+		//bool _keyThree = Input.GetKeyDown(KeyCode.Alpha3);
+		//bool _keyFour = Input.GetKeyDown(KeyCode.Alpha4);
 
 		if(_keyOne){
-			_interactionMachine.SetState(InteractionStates.DayNight);
+			//_interactionMachine.SetState(InteractionStates.DayNight);
+			NextState();
 		}
 		if(_keyTwo){
-			_interactionMachine.SetState(InteractionStates.HiddenLayer);
+			//_interactionMachine.SetState(InteractionStates.HiddenLayer);
+			PreviousState();
 		}
-		if(_keyThree){
+		/*if(_keyThree){
 			_interactionMachine.SetState(InteractionStates.Magnifier);
 		}
 		if(_keyFour){
 			_interactionMachine.SetState(InteractionStates.Scaler);
-		}
+		}*/
     }
+
+	void NextState(){
+		var currentScreen = _uiSystemScreenRotation.NextScreen();
+		var currentState = _interactionStates[currentScreen.name];
+		_interactionMachine.SetState(currentState);
+	}
+
+	void PreviousState(){
+		var previousScreen = _uiSystemScreenRotation.PreviousScreen();
+		var previousState = _interactionStates[previousScreen.name];
+		_interactionMachine.SetState(previousState);
+	}
 }
