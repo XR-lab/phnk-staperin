61ff3005c362a9f241c23eace2b46fe394b93951
Tommiee <tomdepom1999@gmail.com>
2019-03-15 11:17:20 +0100

    Cleanup
    Removed a bunch of code commented out
    Made NextState and PreviousState Public, to facilitate timer access

--- Assets/Scripts/UI/KeyboardChangeState.cs
+++ Assets/Scripts/UI/KeyboardChangeState.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CM.UI;
 
 public class KeyboardChangeState : MonoBehaviour
 {
-	//private readonly StateMachine<InteractionStates> _interactionMachine = new StateMachine<InteractionStates>();
-	[SerializeField] private InteractionMachine _interactionMachine;
+	[SerializeField] public InteractionMachine _interactionMachine;
 	private CM_UI_System_ScreenRotation _uiSystemScreenRotation;
 	private Dictionary<string, InteractionStates> _interactionStates = new Dictionary<string, InteractionStates>(){
 		{ "DayNightUI", InteractionStates.DayNight},
@@ -22,36 +22,28 @@ public class KeyboardChangeState : MonoBehaviour
     void Update(){
 		bool _keyOne = Input.GetKeyDown(KeyCode.Alpha1);
 		bool _keyTwo = Input.GetKeyDown(KeyCode.Alpha2);
-		//bool _keyThree = Input.GetKeyDown(KeyCode.Alpha3);
-		//bool _keyFour = Input.GetKeyDown(KeyCode.Alpha4);
 
 		if(_keyOne){
-			//_interactionMachine.SetState(InteractionStates.DayNight);
-			NextState();
+			PreviousState();
 		}
 		if(_keyTwo){
-			//_interactionMachine.SetState(InteractionStates.HiddenLayer);
-			PreviousState();
-		}/*
-		if(_keyThree){
-			_interactionMachine.SetState(InteractionStates.Magnifier);
+			NextState();
 		}
-		if(_keyFour){
-			_interactionMachine.SetState(InteractionStates.Scaler);
-		}*/
     }
 
-	private void NextState(){
+	//Switches to the next state in order in the state machine
+	public void NextState(){
 		var currentScreen = _uiSystemScreenRotation.NextScreen();
 		var currentState = _interactionStates[currentScreen.name];
 		_interactionMachine.SetState(currentState);
-		print("Switched to: " + currentState);
+		//print("Switched to: " + currentState);
 	}
 
-	private void PreviousState(){
+	//Switches to the previous state in order in the state machine
+	public void PreviousState(){
 		var previousScreen = _uiSystemScreenRotation.PreviousScreen();
 		var previousState = _interactionStates[previousScreen.name];
-		print("switching to: " + previousState);
 		_interactionMachine.SetState(previousState);
+		//print("Switched to: " + previousState);
 	}
 }
