ac57155653343a66272a35ac4839db588a0058df
lucasmuileboom <muileboomlucas@gmail.com>
2019-03-19 15:28:49 +0100

    added timer + timer manager

---
+++ Assets/Scripts/Timer/TimerClass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerClass : MonoBehaviour
+{
+	public delegate void TimerCompletedEvent();
+	public event TimerCompletedEvent TimerCompleted;
+
+	private float _seconds;
+	public float GetSeconds()
+	{
+		return _seconds;
+	}
+
+	private float _threshold;
+	public float GetThreshold()
+	{
+		return _threshold;
+	}
+
+	public void StartTimer(float sec)
+	{
+        _threshold = sec;
+		StartCoroutine(Timer(sec));
+	}
+
+	IEnumerator Timer(float threshold)
+	{
+        _seconds = 0;
+		while (_seconds < threshold)
+		{
+            _seconds += Time.deltaTime;	
+			yield return null;
+		}
+        if (TimerCompleted != null)
+        {
+            TimerCompleted();
+        }		
+	}
+}
---
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    [SerializeField] private ReadData ReadData;
+    [SerializeField] private KeyboardChangeState KeyboardChangeState;
+    private TimerClass ChangeStateTimer;
+    private TimerClass gameTimer;
+
+    void Start()
+    {
+        ChangeStateTimer = gameObject.AddComponent<TimerClass>();
+        ChangeStateTimer.TimerCompleted += KeyboardChangeState.NextState;
+        ChangeStateTimer.StartTimer(ReadData.GetTransferTime());
+        
+        gameTimer = gameObject.AddComponent<TimerClass>();
+        gameTimer.TimerCompleted += GameTimeDone;
+        gameTimer.StartTimer(ReadData.GetSeconds());
+    }
+    void TimeLeft()
+    {//fade in/out pop-up met als text: 1 minuut left
+        print("1 min left");
+    }
+    void GameTimeDone()
+    {//fade in zwart scherm met text : bedankt voor spelen (hou de trekker vast om te starten)
+        print("time up");
+    }
+}
