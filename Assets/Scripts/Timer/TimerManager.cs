6198c02b82659984e0c912c9cff1d43159383ef9
lucasmuileboom <muileboomlucas@gmail.com>
2019-03-20 15:38:22 +0100

    added fade in/out

--- Assets/Scripts/Timer/TimerManager.cs
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -1,30 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerManager : MonoBehaviour
 {
-    [SerializeField] private ReadData ReadData;
-    [SerializeField] private KeyboardChangeState KeyboardChangeState;
+    [SerializeField] private Fade _fade;
+    //[SerializeField] private ReadData ReadData;
+    //[SerializeField] private KeyboardChangeState KeyboardChangeState;
+    [SerializeField] private Text _bedanktText;
+    [SerializeField] private Text _resetText;
+    [SerializeField] private Text _timeLeftText;
+    [SerializeField] private Image _canvasImage;
     private TimerClass ChangeStateTimer;
     private TimerClass gameTimer;
 
     void Start()
     {
+        /*
         ChangeStateTimer = gameObject.AddComponent<TimerClass>();
         ChangeStateTimer.TimerCompleted += KeyboardChangeState.NextState;
-        ChangeStateTimer.StartTimer(ReadData.GetTransferTime());
-        
+        ChangeStateTimer.TimerCompleted += ResetChangeStateTimer;
+        ChangeStateTimer.StartTimer(ReadData.GetTransferTime());*/
+
         gameTimer = gameObject.AddComponent<TimerClass>();
-        gameTimer.TimerCompleted += GameTimeDone;
-        gameTimer.StartTimer(ReadData.GetSeconds());
+        gameTimer.TimerCompleted += TimeLeft;
+        gameTimer.StartTimer(10/*ReadData.GetSeconds() - 60*/);
+
+        _fade.FadeOutInstantly(_timeLeftText);
+        _fade.FadeOutInstantly(_resetText);
+        _fade.FadeOutInstantly(_bedanktText);
+        _fade.FadeOutInstantlyImage(_canvasImage);
+    }
+    void ResetChangeStateTimer()
+    {
+       // ChangeStateTimer.StartTimer(ReadData.GetTransferTime());
     }
     void TimeLeft()
-    {//fade in/out pop-up met als text: 1 minuut left
+    {
         print("1 min left");
+        _fade.FadeOutAndIn(_timeLeftText);
+
+        gameTimer.TimerCompleted -= TimeLeft;
+        gameTimer.TimerCompleted += GameTimeDone;
+        gameTimer.StartTimer(60);
     }
     void GameTimeDone()
-    {//fade in zwart scherm met text : bedankt voor spelen (hou de trekker vast om te starten)
+    {//hij moet nu kunnen reseten door 1 sec op de trigger te klicken
         print("time up");
+        _fade.FadeIn(_resetText);
+        _fade.FadeIn(_bedanktText);
+        _fade.FadeInImage(_canvasImage);
     }
 }
---
+++ Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public void FadeOutInstantlyImage(Image image)
+    {
+        image.CrossFadeAlpha(0, 0f, false);
+    }
+    public void FadeInImage(Image image)
+    {
+        image.CrossFadeAlpha(1, 2.0f, false);
+    }
+
+    public void FadeIn(Text text)
+    {
+        text.CrossFadeAlpha(1, 2.0f, false);
+    }
+    public void FadeOut(Text text)
+    {
+        text.CrossFadeAlpha(0, 2.0f, false);
+    }
+    public void FadeOutInstantly(Text text)
+    {
+        text.CrossFadeAlpha(0, 0f, false);
+    }
+    public void FadeOutAndIn(Text text)
+    {
+        StartCoroutine(FadeInAndOut(text));
+    }
+    IEnumerator FadeInAndOut(Text text)
+    {
+        text.CrossFadeAlpha(1, 2.0f, false);
+        yield return new WaitForSeconds(2);
+        text.CrossFadeAlpha(0, 2.0f, false);
+    }
+}
+
