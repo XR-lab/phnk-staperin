1f1b78b83e369be016ba64d24b3459a589c1ed63
PractoraatHetNieuweKijken <tomdepom1999@gmail.com>
2019-06-12 14:10:35 +0200

    adds default quality setting

---
+++ Assets/Scripts/Utils/QualitySetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualitySetter : MonoBehaviour
+{
+
+    [SerializeField]
+    private string _defaultQuality = "Ultra";
+    void Start() {
+        string[] names = QualitySettings.names;
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (names[i] == _defaultQuality)
+            {
+                QualitySettings.SetQualityLevel(i, true);
+                return;
+            }
+        }        
+    }
+}
